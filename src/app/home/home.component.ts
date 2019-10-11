bd565ec4d0d40ce2ec8fb70d1d84240194c62bf0
actionanand <meetanandns@gmail.com>
2019-10-11 12:15:55 +0530

    interval observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../guards/auth.service';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   login:boolean;
+  homeSubscription: Subscription;
 
   constructor(private router: Router, private authservice: AuthService){}
   ngOnInit(){
     this.login = this.authservice.loggedIn;
+
+   this.homeSubscription = interval(1000).subscribe({
+     next: count =>{
+        console.log(count);
+      }
+    });
+  }
+
+  ngOnDestroy(){
+    this.homeSubscription.unsubscribe();
   }
 
   onLoadServers(){
@@ -29,5 +41,7 @@ export class HomeComponent implements OnInit {
     this.authservice.logout();
     this.login = this.authservice.loggedIn;
   }
+
+
 
 }
