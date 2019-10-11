15859cbc9bd35cf0b1f51878c5956d247a8e56fb
actionanand <meetanandns@gmail.com>
2019-10-11 12:27:47 +0530

    custom observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../guards/auth.service';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -17,11 +17,24 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnInit(){
     this.login = this.authservice.loggedIn;
 
-   this.homeSubscription = interval(1000).subscribe({
-     next: count =>{
-        console.log(count);
-      }
+  //  this.homeSubscription = interval(1000).subscribe({
+  //    next: count =>{
+  //       console.log(count);
+  //     }
+  //   });
+
+    const customObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(()=> {
+        observer.next(count);
+        count++;
+      },1000);
     });
+
+    this.homeSubscription = customObservable.subscribe(data => {
+      console.log(data);
+    });
+
   }
 
   ngOnDestroy(){
